fac144580ce4ea2e53e865b0ecd29e9b461f6592
Woo-Yong0405 <idesign93@gmail.com>
2021-09-22 18:52:47 +0900

    STUN servers added

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -79,6 +79,11 @@ async function getMedia(deviceId) {
     
     async function handleCameraChange() {
         await getMedia(camerasSelect.value);
+        if (myPeerConnection) {
+            const videoTrack = myStream.getVideoTracks()[0];
+            const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === "video");
+            videoSender.replaceTrack(videoTrack);
+        }
     }
     
     muteBtn.addEventListener("click", handleMuteClick);
@@ -122,6 +127,10 @@ socket.on("offer", async (offer) => {
     const answer = await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+});
+
+socket.on("ice", (ice) => {
+    myPeerConnection.addIceCandidate(ice);
 })
 
 socket.on("answer",  (answer) => {
@@ -130,8 +139,29 @@ socket.on("answer",  (answer) => {
 
 //RTC Code
 function makeConnection() {
-    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: {
+            urls: [
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302"
+            ]
+        }
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream.getTracks().forEach(track => {
         myPeerConnection.addTrack(track, myStream);
     });
+}
+
+function handleIce(data) {
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+    const peerStream = document.getElementById("peersStream");
+    peerStream.srcObject = data.stream;
 }
